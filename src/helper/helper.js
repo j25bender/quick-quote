4c725a5044e070c8c341a1a0d21de88371f49860
Jeff Bender <j25bender@gmail.com>
2018-02-25 16:12:48 -0700

    added scrollLeft to nav

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,6 +1,5 @@
 
 export const scrollLeft = (quotes) => { 
-  console.log('hey')
   const scrollElement = document.getElementById('myDiv');
   scrollElement.scrollLeft = quotes.length * window.screen.width;
 }
